b43a8569e85ca42815c1acece509362d1191f80c
Eduardo Henrique <dudugama03@gmail.com>
2025-04-18 16:04:03 -0300

    Mudando texto do frontend do streamlit

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,21 +1,21 @@
 import streamlit as st
 from crew import PlanejamentoCrew
 
-st.title("🔎  Bem Vindo ao seu assitente de nutrição e esporte")
+st.title("🔎  Bem Vindo ao seu assitente de IA de nutrição e esporte")
 
 with st.sidebar:
     st.header("Crie seu planejamento mensal de nutrição e treinamento:")
     peso = st.text_input("Qual seu peso atual")
     imc = st.text_input("Qual seu IMC (Índice massa corporal)")
     objetivo = st.text_area("Qual seu objetivo na dieta e qual seu objetivo no esporte?")
-    tempo_treinamento = st.text_input("Quuanto tempo de treinamento você possui?")
-    esportes = st.text_input("Quais esportes você deseja praticar e qual a frequência?")
+    tempo_treinamento = st.text_area("Quanto tempo de treinamento você possui?")
+    esportes = st.text_area("Quais esportes você deseja praticar e qual a frequência?")
     
-if st.button("Executar pergunta 🚀"):
+if st.button("Gerar Planejamento 🚀"):
     if not peso or not imc or not objetivo or not tempo_treinamento or not esportes:
         st.warning("⚠️Preencha todos os campos.")
     else:
-        st.write("⏳ Processando sua pergunta... Aguarde.")
+        st.write("⏳ Gerando o seu planejamento... Aguarde")
 
         research_crew = PlanejamentoCrew(
             peso=peso, 
@@ -27,5 +27,6 @@ if st.button("Executar pergunta 🚀"):
         
         result = research_crew.run()
         st.subheader("Aqui está seu planejamento")
+         
         for txt in result:
             st.markdown(txt)
